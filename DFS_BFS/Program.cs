d4ab1d485c96ac8d39eb50d8f4fbe32e07eed30e
Mohammad Abdus Salam <asalam345@gmail.com>
2019-07-09 12:25:33 +0600

    Another Program With DFS BFS

--- DFS_BFS/Program.cs
+++ DFS_BFS/Program.cs
@@ -6,137 +6,195 @@ using System.Threading.Tasks;
 
 namespace DFS_BFS
 {
-	public class Node
-	{
-		public Node()
-		{
-			Neighbours = new List<Node> ();
-		}
-
-		public int Value { get; set; }
-		public IList<Node> Neighbours { get; set; }
-	}
 	class Program
 	{
-		static void Main(string[] args)
-		{
-			Node topNode = new Node() { Value = 1 };
-			topNode.Neighbours.Add(new Node() { Value = 2, Neighbours = new List<Node> { new Node() { Value = 5, Neighbours = new List<Node> { new Node() { Value = 9 } } }	}});
-			topNode.Neighbours.Add(new Node() { Value = 3, Neighbours = new List<Node> { new Node() { Value = 6, Neighbours = new List<Node> () { new Node() { Value = 10 } } }, new Node() { Value = 7 } }});
-			topNode.Neighbours.Add(new Node() { Value = 4, Neighbours = new List<Node> () { new Node() { Value = 8 } } });
- 
- 
-			Queue<Node> queue = new Queue<Node>();
-			queue.Enqueue(topNode);
- 
-			while (queue.Count > 0)
+		
+		public static void Main(string[] args)
 			{
-			   var element = queue.Dequeue();
-				if (element.Neighbours.Count > 0)
+				// Create a graph given in the above diagram
+				var g = new Graph();
+
+				g.AddEdge(1, 2);
+				g.AddEdge(1, 3);
+				g.AddEdge(1, 4);
+
+				g.AddEdge(2, 5);
+				g.AddEdge(2, 6);
+				g.AddEdge(4, 7);
+				g.AddEdge(4, 8);
+
+				g.AddEdge(5, 9);
+				g.AddEdge(5, 10);
+				g.AddEdge(7, 11);
+				g.AddEdge(7, 12);
+
+
+				Console.WriteLine("Following is Breadth First Traversal (starting from vertex 1)");
+				g.BFSWalkWithStartNode(1);
+
+				Console.WriteLine("Following is Depth First Traversal (starting from vertex 1)");
+				g.DFSWalkWithStartNode(1);
+
+				Console.WriteLine("Following is Depth First Traversal USING RECURSION (starting from vertex 1)");
+				g.DFSWithRecursion(1);
+
+				Console.ReadKey();
+			}
+
+		public class Graph
+		{
+			public Graph()
+			{
+				Adj = new Dictionary<int, HashSet<int>>();
+			}
+
+			public Dictionary<int, HashSet<int>> Adj { get; private set; }
+
+			public void AddEdge(int source, int target)
+			{
+				if (Adj.ContainsKey(source))
 				{
-					Console.WriteLine(element.Value);
-					foreach (var item in element.Neighbours)
+					try
 					{
-						queue.Enqueue(item);
+						Adj[source].Add(target);
+					}
+					catch
+					{
+						Console.WriteLine("This edge already exists: " + source + " to " + target);
 					}
 				}
 				else
-					Console.WriteLine(element.Value);
-			}
-
-
-
-
-			//type 2
-			var g = new Graph<int>();
-			var n1 = new Node<int>(1);
-			var n2 = new Node<int>(2);
-			var n3 = new Node<int>(3);
-			var n4 = new Node<int>(4);
-			var n5 = new Node<int>(5);
-			var n6 = new Node<int>(6);
-			var n7 = new Node<int>(7);
-			g.AddEdge(n1, n2);
-			g.AddEdge(n1, n3);
-			g.AddEdge(n1, n4);
-			g.AddEdge(n4, n5);
-			g.AddEdge(n2, n6);
-			g.AddEdge(n4, n7);
-			g.AddEdge(n5, n6);
-			g.AddEdge(n6, n7);
-
-			Console.WriteLine("Shortest path is: {0}", string.Join("->", g.ShortestPath(n3, n5)));
-
-			Console.Read(); 
-		}
-
-		//another process
-		public class Node<T>
-		{
-			public T Value;
-
-			public Node(T val)
-			{
-				Value = val;
-			}
-
-			public override string ToString()
-			{
-				return Value.ToString();
-			}
-		}
-
-		public class Graph<T>
-		{
-			//private readonly List<Node<T>> _nodes;
-			private readonly Dictionary<Node<T>, List<Node<T>>> _adj;
-
-			public Graph()
-			{
-				//_nodes = new List<Node<T>>();
-				_adj = new Dictionary<Node<T>, List<Node<T>>>();
-			}
-
-			public void AddEdge(Node<T> node1, Node<T> node2)
-			{
-				if (!_adj.ContainsKey(node1))
-					_adj[node1] = new List<Node<T>>();
-				if (!_adj.ContainsKey(node2))
-					_adj[node2] = new List<Node<T>>();
-				_adj[node1].Add(node2);
-				_adj[node2].Add(node1);
-			}
-
-			public Stack<Node<T>> ShortestPath(Node<T> source, Node<T> dest)
-			{
-				var path = new Dictionary<Node<T>, Node<T>>();
-				var distance = new Dictionary<Node<T>, int>();
-				foreach (var node in _adj.Keys)
 				{
-					distance[node] = -1;
+					var hs = new HashSet<int>();
+					hs.Add(target);
+					Adj.Add(source, hs);
 				}
-				distance[source] = 0;
-				var q = new Queue<Node<T>>();
-				q.Enqueue(source);
+			}
+
+			public void BFSWalkWithStartNode(int vertex)
+			{
+				var visited = new HashSet<int>();
+				// Mark this node as visited
+				visited.Add(vertex);
+				// Queue for BFS
+				var q = new Queue<int>();
+				// Add this node to the queue
+				q.Enqueue(vertex);
+
 				while (q.Count > 0)
 				{
-					var node = q.Dequeue();
-					foreach (var adj in _adj[node].Where(n => distance[n] == -1))
+					var current = q.Dequeue();
+					Console.WriteLine(current);
+					// Only if the node has a any adj notes
+					if (Adj.ContainsKey(current))
 					{
-						distance[adj] = distance[node] + 1;
-						path[adj] = node;
-						q.Enqueue(adj);
+						// Iterate through UNVISITED nodes
+						foreach (int neighbour in Adj[current].Where(a => !visited.Contains(a)))
+						{
+							visited.Add(neighbour);
+							q.Enqueue(neighbour);
+						}
 					}
 				}
-				var res = new Stack<Node<T>>();
-				var cur = dest;
-				while (cur != source)
+			}
+
+			public int BFSFindNodeWithStartNode(int vertex, int lookingFor)
+			{
+				if (vertex == lookingFor)
 				{
-					res.Push(cur);
-					cur = path[cur];
+					Console.WriteLine("Found it!");
+					Console.WriteLine("Steps Took: 0");
+					return 0;
 				}
-				res.Push(source);
-				return res;
+				var visited = new HashSet<int>();
+				// Mark this node as visited
+				visited.Add(vertex);
+				// Queue for BFS
+				var q = new Queue<int>();
+				// Add this node to the queue
+				q.Enqueue(vertex);
+
+				int count = 0;
+
+				while (q.Count > 0)
+				{
+					var current = q.Dequeue();
+					Console.WriteLine(current);
+					if (current == lookingFor)
+					{
+						Console.WriteLine("Found it!");
+						Console.WriteLine("Steps Took: " + count);
+						return visited.Count();
+					}
+
+					// Only if the node has a any adj notes
+					if (Adj.ContainsKey(current))
+					{
+						// Iterate through UNVISITED nodes
+						foreach (int neighbour in Adj[current].Where(a => !visited.Contains(a)))
+						{
+							visited.Add(neighbour);
+							q.Enqueue(neighbour);
+						}
+					}
+					count++;
+				}
+				Console.WriteLine("Could not find node!");
+				return count;
+			}
+
+			public void DFSWalkWithStartNode(int vertex)
+			{
+				var visited = new HashSet<int>();
+				// Mark this node as visited
+				visited.Add(vertex);
+				// Stack for DFS
+				var s = new Stack<int>();
+				// Add this node to the stack
+				s.Push(vertex);
+
+				while (s.Count > 0)
+				{
+					var current = s.Pop();
+					Console.WriteLine(current);
+					// ADD TO VISITED HERE
+					if (!visited.Contains(current))
+					{
+						visited.Add(current);
+					}
+					// Only if the node has a any adj notes
+					if (Adj.ContainsKey(current))
+					{
+						// Iterate through UNVISITED nodes
+						foreach (int neighbour in Adj[current].Where(a => !visited.Contains(a)))
+						{
+							visited.Add(neighbour);
+							s.Push(neighbour);
+						}
+					}
+				}
+			}
+
+			public void DFSWithRecursion(int vertex)
+			{
+				var visited = new HashSet<int>();
+				Traverse(vertex, visited);
+			}
+
+			private void Traverse(int v, HashSet<int> visited)
+			{
+				// Mark this node as visited
+				visited.Add(v);
+				Console.WriteLine(v);
+				// Only if the node has a any adj notes
+				if (Adj.ContainsKey(v))
+				{
+					// Iterate through UNVISITED nodes
+					foreach (int neighbour in Adj[v].Where(a => !visited.Contains(a)))
+					{
+						Traverse(neighbour, visited);
+					}
+				}
 			}
 		}
 	}
